6dacf3dadef5235c10966b491d381f34c86e6c62
rishabh <rishabh singh>
2020-08-06 22:03:35 +0530

    multiple actions done

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const redux = require('redux');
-const createStore = redux.createStore
+const createStore = redux.createStore;
 
-const buy_bottle = 'buy the bottle'
+const buy_bottle = 'buy the bottle';
+const buy_packets = 'buy the packets';
 
 const initialState = {
     numberOfBottles:10,
+    numberOfPackets:20,
     tagOnBottles :"Teachers"
 }
 
+
+// action creators
 function buyBottles(){
     return{
+        // actions
         type:buy_bottle,
         info:"not my first redux code"
     }
 }
-console.log(buyBottles())
+
+function buyPackets(){
+    return{
+        type:buy_packets,
+        payload:"not my first redux code"
+    }
+}
 
 const Reducer = (state=initialState,action)=>{
     switch(action.type){
@@ -23,6 +34,11 @@ const Reducer = (state=initialState,action)=>{
             ...state,
             numberOfBottles:state.numberOfBottles-1
         }
+        case buy_packets:
+            return{
+                ...state,
+                numberOfPackets:state.numberOfPackets-2
+            }
         default:return state
     }
 }
@@ -31,5 +47,6 @@ const store=createStore(Reducer)
 console.log('initial state',store.getState())
 const unsuscribe = store.subscribe(()=>{console.log('updated state',store.getState())})
 store.dispatch(buyBottles())
+store.dispatch(buyPackets())
 
 unsuscribe()
